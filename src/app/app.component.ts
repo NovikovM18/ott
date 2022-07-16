7b6854f3ca0534cef12ba7285e36034a01f36d6b
NovikovM <novikov1807@gmail.com>
2022-07-16 13:03:14 +0300

    make first comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ott';
+export class AppComponent implements OnInit {
+  formData!: FormGroup;
+  animations = ['sdfsdf', 'sdfsdfdsf', 'sdfsdfsdf']
+  imagePreview: any;
+  show = false;
+  left!: FormControl;
+  top!: FormControl;
+  
+  constructor() {}
+
+  transformImage() {
+    console.log(this.left.value);
+    console.log(this.top.value);
+
+    const image = document.getElementById('image');
+    if(image) {
+      image.style.left = this.left.value + 'px';
+      image.style.top = this.top.value + 'px';
+    }
+
+  }
+
+f(event: any) {
+  console.log(event.target.value);
+  const image = document.getElementById('image');
+  if(image) {
+    image.style.transform = 'scale('+event.target.value/10+')';
+  }
+  
+}
+  ngOnInit(): void {
+    this.left = new FormControl(0);
+    this.top = new FormControl(0);
+
+
+    this.formData = new FormGroup({
+      image: new FormControl(null, [Validators.required]),
+      url: new FormControl('', [Validators.required, Validators.pattern(/^(?:http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/)]),
+      animation: new FormControl('', [Validators.required]),
+    })
+  }
+
+  uploadFile(event: any) {
+    const file = event.target.files[0];
+    this.formData.patchValue({image: file});
+    this.formData.get('image')?.updateValueAndValidity();
+  }
+
+  submitedForm() {
+    if (this.formData.valid) {
+      this.show = true;
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagePreview = reader.result;
+      }
+      reader.readAsDataURL(this.formData.value.image);
+    }
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSliderModule} from '@angular/material/slider';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
